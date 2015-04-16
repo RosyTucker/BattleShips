74ce2a533969c5bad793d86840ea2a3420ee278e
Rosy Tucker <rtucker@thoughtworks.com>
2015-04-16 14:47:18 +0100

    Switch to web sockets

--- BattleShipsAPI/BattleShipsAPI.ino
+++ BattleShipsAPI/BattleShipsAPI.ino
@@ -1,45 +1,54 @@
 #include <SPI.h>
 #include <Ethernet.h>
-//Server
-EthernetClient client;
-byte serverIP[] = { 10, 93, 20, 76 };
-String server = "10.93.20.76";
-int serverPort = 9456;
-int buttonPin = 12;
-int setupLedPin = 8;
-int moveLedPin = 2;
-bool isSetup = false;
+#include <WebSocketClient.h>
 
-//Your Arduino
+//Server
+byte server[] = { 10,93,20,76 };
+int port = 2999;
+char hostString[] = "10.93.20.76:2999";
+EthernetClient client;
+WebSocketClient webSocketClient;
+
+
+//Arduino
+int transmitPin = 8;
+int errorPin = 2;
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xBC, 0xAA };
 
 void setup() {
-  randomSeed(analogRead(0));
-  configureSerial();
-  configureEthernet();
-  pinMode(buttonPin, INPUT);
-  pinMode(setupLedPin, OUTPUT);
-  pinMode(moveLedPin, OUTPUT);
+  pinMode(transmitPin, OUTPUT);
+  pinMode(errorPin, OUTPUT);
+  setupSerial();
+  setupEthernet(mac, server, port);
+  setupWebSocket(hostString, client);
 }
 
-void loop() {  
-  if(digitalRead(buttonPin) == HIGH){
-      digitalWrite(setupLedPin, HIGH);
-      delay(200);
-      isSetup = true;
-      Serial.println("ButtonPressed");
-      String boatsJson = "{\"boats\":[{\"s\":\"0,0\",\"e\":\"0,4\"},{\"s\":\"2,4\",\"e\":\"2,5\"},{\"s\":\"9,0\",\"e\":\"9,2\"},{\"s\":\"6,7\",\"e\":\"6,9\"},{\"s\":\"5,6\",\"e\":\"8,6\"}]}";
-      postJSON("/setup", boatsJson);
-      digitalWrite(setupLedPin, LOW);
-  }else if(isSetup == true){
-       digitalWrite(moveLedPin, HIGH);
-       int xValue = random(0,10);
-       int yValue = random(0,10);
-       String moveJson = createMove(xValue, yValue); 
-       postJSON("/makeMove", moveJson);
-       digitalWrite(moveLedPin, LOW);
-       delay(200);
-   }
+void loop() {
+  if (client.connected()) {
+    digitalWrite(errorPin, HIGH);
+    digitalWrite(transmitPin, HIGH);
+    sendMove();
+    digitalWrite(transmitPin, LOW);
+  } else {
+    Serial.println("Client disconnected.");
+    digitalWrite(errorPin, HIGH);
+  }
+  delay(2000);
+}
+
+void recieveMove() {
+  String data;
+  webSocketClient.getData(data);
+  if (data.length() > 0) {
+    Serial.print("Received data: ");
+    Serial.println(data);
+  }
+}
+void sendMove() { 
+    int xValue = random(0,10);
+    int yValue = random(0,10);
+    String moveJson = createMove(xValue, yValue); 
+    webSocketClient.sendData(moveJson);
 }
 
 String createMove(int xValue, int yValue) {
@@ -50,41 +59,40 @@ String createMove(int xValue, int yValue) {
   return fullMoveString;
 }
 
-void postJSON(String route, String jsonData) {
-    if (connectClient()) {
-       client.println("POST " + route + " HTTP/1.1");
-       client.println(createRequestMetaData(serverPort, jsonData));
-       client.println();
-       client.println();
-       client.println(jsonData);
-       client.stop();
-    }
-}
+/*****************************************/
+/*                Helpers                */
+/******************************************/ 
 
-
-String createRequestMetaData(int serverPort, String data){
-    return  "Host: " + server + ":" + String(serverPort) + "\r\nConnection: close\r\nAccept: */*\r\nContent-Type: application/json\r\nContent-Length: " + String(data.length());
-}
-
-void configureSerial() {
+void setupSerial() {
     Serial.begin(9600);
     while (!Serial) {
       ;
-    }
+    } 
 }
 
-void configureEthernet() {
-    if (Ethernet.begin(mac) == 1) {
+void setupEthernet(byte mac[], byte server[], int port) {
+  if (Ethernet.begin(mac) == 1) {
         Serial.print("Configured Ethernet with IP: ");
         Serial.println(Ethernet.localIP());
-        connectClient();
+        client.connect(server, port);
         delay(1000);
     }else{
         Serial.println("Could not configure Ethernet");
+        while(1) {
+            ;
+        }  
     }
 }
 
-bool connectClient() {
-   client.connect(serverIP, serverPort);
-   return client.connected();
+void setupWebSocket(String host, EthernetClient client) {
+  webSocketClient.path = "/";
+  webSocketClient.host = hostString;
+  if (webSocketClient.handshake(client)) {
+    Serial.println("Handshake successful");
+  } else {
+    Serial.println("Handshake failed");
+    while(1) {
+      // Hang on failure
+    }  
+  }
 }
