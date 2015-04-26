fdbf06c0d44c03f05fca6674a3b912551cd7f5ea
Rosy Tucker <rtucker@thoughtworks.com>
2015-04-26 10:11:23 +0100

    Change structure of boat
    Now has length and direction rather than start and end

--- models/boat.rb
+++ models/boat.rb
@@ -1,38 +1,32 @@
 require_relative 'grid_position'
 class Boat
 
-  attr_reader :start_position
-  attr_reader :position
-
-  def initialize start_position, end_position
+  def initialize start_position, length, direction
     @start_position = start_position
-    @end_position = end_position
+    @length = length
+    @direction = direction
   end
 
   def self.from_json_object json_object
-    start_coords = json_object['s'].split(',')
-    end_coords = json_object['e'].split(',')
+    start_coords = json_object['start'].split(',')
+    length = json_object['length']
+    direction = json_object['direction']
     start_position = GridPosition.new start_coords[0].to_i, start_coords[1].to_i
-    end_position = GridPosition.new end_coords[0].to_i, end_coords[1].to_i
-    Boat.new start_position, end_position
+    Boat.new start_position, length, direction
   end
 
   def contains grid_position
-    if contains_x(grid_position) && contains_y(grid_position)
-      true
-    else
-      false
+    case @direction
+      when 'E'
+        (grid_position.x_pos >= @start_position.x_pos && grid_position.x_pos < @start_position.x_pos + @length)
+      when 'W'
+        (grid_position.x_pos <= @start_position.x_pos && grid_position.x_pos > @start_position.x_pos - @length)
+      when 'N'
+        (grid_position.y_pos <= @start_position.y_pos && grid_position.y_pos > @start_position.y_pos - @length)
+      when 'S'
+        (grid_position.y_pos >= @start_position.y_pos && grid_position.y_pos < @start_position.y_pos + @length)
+      else
+        false
     end
-  end
-
-  private
-  def contains_x(grid_position)
-    (grid_position.x_pos <= @end_position.x_pos && grid_position.x_pos >= @start_position.x_pos) ||
-        (grid_position.x_pos >= @end_position.x_pos && grid_position.x_pos <= @start_position.x_pos)
-  end
-
-  def contains_y(grid_position)
-    (grid_position.y_pos <= @end_position.y_pos && grid_position.y_pos >= @start_position.y_pos) ||
-        (grid_position.y_pos >= @end_position.y_pos && grid_position.y_pos <= @start_position.y_pos)
   end
 end
