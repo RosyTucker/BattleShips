86ccc7b37377f62e2633bb3115ef574ebeaf998c
Rosy Tucker <rtucker@thoughtworks.com>
2015-04-26 16:55:08 +0100

    Format Arduino code

--- Arduino/BattleShips/BattleShips.ino
+++ Arduino/BattleShips/BattleShips.ino
@@ -11,35 +11,32 @@ EthernetClient client;
 WebSocketClient webSocketClient;
 
 //Arduino
-int transmitPin = 2;
-int errorPin = 8;
-int buttonPin = 9;
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xBC, 0xAA };
+int transmitPin = 2;  // Connent an LED to this pin
+int errorPin = 8;    // Connect an LED to this pin 
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xBC, 0xAA }; //CHANGE THIS
 
 // Game
 bool registered = false;
-char trueString[] = "true";
 
 void setup() {
   pinMode(transmitPin, OUTPUT);
   pinMode(errorPin, OUTPUT);
-  pinMode(buttonPin, INPUT);
   setupSerial();
   setupEthernet();
   setupWebSocket();
 }
 
 void loop() {
+  digitalWrite(errorPin, LOW);
   if (client.connected()) {
-    digitalWrite(errorPin, LOW);
     if(!registered) {
       Serial.println("Printing");
       digitalWrite(transmitPin, HIGH);
       registerAsPlayer();
+      digitalWrite(transmitPin, LOW);
       if(!getIsFirstFromStartMessage(recieveData())){
         String otherPlayersMove = recieveData();
       }
-      digitalWrite(transmitPin, LOW);
     } else {
       digitalWrite(transmitPin, HIGH);
       sendMove();
@@ -57,7 +54,7 @@ void loop() {
 void registerAsPlayer() {
   String registration = "{\"type\":\"register\",\"data\":{\"boats\":[{\"start\":\"2,3\", \"length\": 3, \"direction\": \"S\"}, {\"start\":\"5,6\", \"length\": 5, \"direction\": \"N\"}]}}";
   webSocketClient.sendData(registration);
-  delay(2000);
+  delay(1000);
   registered = true;
 }
 
@@ -75,13 +72,13 @@ String recieveData() {
   return data;
 }
 void sendMove() { 
-    int xValue = random(0,10);
-    int yValue = random(0,10);
-    String moveJson = createMove(xValue, yValue); 
-    Serial.println("Sending:");
-    Serial.println(moveJson);
-    webSocketClient.sendData(moveJson);
-    delay(2000);
+  int xValue = random(0,10);
+  int yValue = random(0,10);
+  String moveJson = createMove(xValue, yValue); 
+  Serial.println("Sending:");
+  Serial.println(moveJson);
+  webSocketClient.sendData(moveJson);
+  delay(1000);
 }
 
 String createMove(int xValue, int yValue) {
@@ -110,24 +107,24 @@ bool getIsFirstFromStartMessage(String startMessageJson) {
 /******************************************/ 
 
 void setupSerial() {
-    Serial.begin(9600);
-    while (!Serial) {
-       digitalWrite(errorPin, HIGH);
-    } 
+  Serial.begin(9600);
+  while (!Serial) {
+     digitalWrite(errorPin, HIGH);
+  } 
 }
 
 void setupEthernet() {
   if (Ethernet.begin(mac) == 1) {
-        Serial.print("Configured Ethernet with IP: ");
-        Serial.println(Ethernet.localIP());
-        client.connect(server, port);
-        delay(1000);
-    }else{
-        Serial.println("Could not configure Ethernet");
-        while(1) {
-           digitalWrite(errorPin, HIGH);
-        }  
-    }
+    Serial.print("Configured Ethernet with IP: ");
+    Serial.println(Ethernet.localIP());
+    client.connect(server, port);
+    delay(1000);
+  }else{
+    Serial.println("Could not configure Ethernet");
+    while(1) {
+       digitalWrite(errorPin, HIGH);
+    }  
+  }
 }
 
 void setupWebSocket() {
